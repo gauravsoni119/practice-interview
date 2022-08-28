c3124d952fdd94de90509284f63c05ee1b5b77d5
gauravsoni119 <sonigaurav119@gmail.com>
2022-08-28 16:13:38 +0200

    style: format code

--- src/ds/avl-tree.ts
+++ src/ds/avl-tree.ts
@@ -19,38 +19,38 @@
  * @param {unknown} val - The value stored in the AV: tree
  */
 export class TreeNode<T> {
-  /**
-   * Value of the tree node.
-   * @member {Object}
-   */
-  public value!: T;
-  /**
-   * Left node
-   * @member {TreeNode}
-   */
-  left: TreeNode<T> | null = null;
-  /**
-   * Right node
-   * @member {TreeNode}
-   */
-  right: TreeNode<T> | null = null;
-  /**
-   * Height of the node
-   * @member {number}
-   */
-  height = 0;
+	/**
+	 * Value of the tree node.
+	 * @member {Object}
+	 */
+	public value!: T;
+	/**
+	 * Left node
+	 * @member {TreeNode}
+	 */
+	left: TreeNode<T> | null = null;
+	/**
+	 * Right node
+	 * @member {TreeNode}
+	 */
+	right: TreeNode<T> | null = null;
+	/**
+	 * Height of the node
+	 * @member {number}
+	 */
+	height = 0;
 
-  constructor(
-    value: T,
-    left: TreeNode<T> | null,
-    right: TreeNode<T> | null,
-    height: number
-  ) {
-    this.value = value;
-    this.left = left;
-    this.right = right;
-    this.height = height;
-  }
+	constructor(
+		value: T,
+		left: TreeNode<T> | null,
+		right: TreeNode<T> | null,
+		height: number
+	) {
+		this.value = value;
+		this.left = left;
+		this.right = right;
+		this.height = height;
+	}
 }
 
 /**
@@ -61,196 +61,196 @@ export class TreeNode<T> {
  * @constructor
  */
 export class AVLTree<T> {
-  root: TreeNode<T> | null = null;
+	root: TreeNode<T> | null = null;
 
-  /**
-   * Insert the node into the AVL tree at given node. If not provided, root will be consider as current. <br><br>
-   * Time Complexity: O(logN) in the average case and O(N) in worst case.
-   *
-   * @public
-   * @method
-   * @param value value that needs to be inserted
-   * @param current Current Node
-   * @returns void
-   */
-  insert(value: T, current: TreeNode<T> | null = null): void {
-    if (!this.root) {
-      this.root = new TreeNode(value, null, null, 1);
-      return;
-    }
-    current = current || this.root;
-    let insertKey: keyof TreeNode<T>;
-    if (current.value > value) {
-      insertKey = 'left';
-    } else {
-      insertKey = 'right';
-    }
-    if (current && !current[insertKey]) {
-      current[insertKey] = new TreeNode(value, null, null, 0);
-    } else {
-      this.insert(value, current[insertKey]);
-    }
-  }
+	/**
+	 * Insert the node into the AVL tree at given node. If not provided, root will be consider as current. <br><br>
+	 * Time Complexity: O(logN) in the average case and O(N) in worst case.
+	 *
+	 * @public
+	 * @method
+	 * @param value value that needs to be inserted
+	 * @param current Current Node
+	 * @returns void
+	 */
+	insert(value: T, current: TreeNode<T> | null = null): void {
+		if (!this.root) {
+			this.root = new TreeNode(value, null, null, 1);
+			return;
+		}
+		current = current || this.root;
+		let insertKey: keyof TreeNode<T>;
+		if (current.value > value) {
+			insertKey = 'left';
+		} else {
+			insertKey = 'right';
+		}
+		if (current && !current[insertKey]) {
+			current[insertKey] = new TreeNode(value, null, null, 0);
+		} else {
+			this.insert(value, current[insertKey]);
+		}
+	}
 
-  /**
-   * Given the root of a binary tree, invert the tree, and return its root.
-   * <br />
-   * For example,
-   * <br />
-   * <b>Input:</b> [4, 2, 7, 1, 3, 6, 9]
-   * <br />
-   * <b>Output:</b> [4, 7, 2, 9, 6, 3, 1]
-   * <br />
-   *
-   * @method
-   * @see {@link https://leetcode.com/problems/invert-binary-tree/|Invert Binary Tree}
-   *
-   * @public
-   *
-   * @returns {TreeNode} Inverted AVL tree
-   */
+	/**
+	 * Given the root of a binary tree, invert the tree, and return its root.
+	 * <br />
+	 * For example,
+	 * <br />
+	 * <b>Input:</b> [4, 2, 7, 1, 3, 6, 9]
+	 * <br />
+	 * <b>Output:</b> [4, 7, 2, 9, 6, 3, 1]
+	 * <br />
+	 *
+	 * @method
+	 * @see {@link https://leetcode.com/problems/invert-binary-tree/|Invert Binary Tree}
+	 *
+	 * @public
+	 *
+	 * @returns {TreeNode} Inverted AVL tree
+	 */
 
-  invert(): TreeNode<T> | null {
-    function invert(root: TreeNode<T> | null) {
-      if (!root) {
-        return null;
-      }
-      const left = root.left;
-      root.left = invert(root.right);
-      root.right = invert(left);
-      return root;
-    }
-    return invert(this.root);
-  }
+	invert(): TreeNode<T> | null {
+		function invert(root: TreeNode<T> | null) {
+			if (!root) {
+				return null;
+			}
+			const left = root.left;
+			root.left = invert(root.right);
+			root.right = invert(left);
+			return root;
+		}
+		return invert(this.root);
+	}
 
-  /**
-   * Given the root of a binary tree, return its maximum depth/height.
-   * <br /><br />
-   * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
-   * For example,
-   * <br />
-   * <b>Input:</b> [3, 9, 20, null, null, 15, 7]
-   * <br />
-   * <b>Output:</b> 3
-   * <br />
-   *
-   * @method
-   * @see {@link https://leetcode.com/problems/maximum-depth-of-binary-tree/|Maximum Depth of Binary Tree}
-   *
-   * @public
-   *
-   * @returns {Number} Diameter of AVL tree
-   */
+	/**
+	 * Given the root of a binary tree, return its maximum depth/height.
+	 * <br /><br />
+	 * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+	 * For example,
+	 * <br />
+	 * <b>Input:</b> [3, 9, 20, null, null, 15, 7]
+	 * <br />
+	 * <b>Output:</b> 3
+	 * <br />
+	 *
+	 * @method
+	 * @see {@link https://leetcode.com/problems/maximum-depth-of-binary-tree/|Maximum Depth of Binary Tree}
+	 *
+	 * @public
+	 *
+	 * @returns {Number} Diameter of AVL tree
+	 */
 
-  getHeight(): number {
-    function maxDepth(root: TreeNode<T> | null): number {
-      if (!root) {
-        return 0;
-      }
-      return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-    }
-    return maxDepth(this.root);
-  }
+	getHeight(): number {
+		function maxDepth(root: TreeNode<T> | null): number {
+			if (!root) {
+				return 0;
+			}
+			return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+		}
+		return maxDepth(this.root);
+	}
 
-  /**
-   * Given the root of a binary tree, return the length of the diameter of the tree
-   * <br /><br />
-   * The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
-   * For example,
-   * <br />
-   * <b>Input:</b> [1, 2, 3, 4, 5]
-   * <br />
-   * <b>Output:</b> 3
-   * <br />
-   *
-   * @method
-   * @see {@link https://leetcode.com/problems/diameter-of-binary-tree/|Diameter of Binary Tree}
-   *
-   * @public
-   *
-   * @returns {Number} Height of AVL tree
-   */
+	/**
+	 * Given the root of a binary tree, return the length of the diameter of the tree
+	 * <br /><br />
+	 * The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+	 * For example,
+	 * <br />
+	 * <b>Input:</b> [1, 2, 3, 4, 5]
+	 * <br />
+	 * <b>Output:</b> 3
+	 * <br />
+	 *
+	 * @method
+	 * @see {@link https://leetcode.com/problems/diameter-of-binary-tree/|Diameter of Binary Tree}
+	 *
+	 * @public
+	 *
+	 * @returns {Number} Height of AVL tree
+	 */
 
-  getDiameter(): number {
-    let ans = 0;
-    function diameterOfBinaryTree(root: TreeNode<T> | null): number {
-      if (!root) {
-        return 0;
-      }
-      const left = diameterOfBinaryTree(root.left);
-      const right = diameterOfBinaryTree(root.right);
-      ans = Math.max(left + right, ans);
-      return 1 + Math.max(left, right);
-    }
-    diameterOfBinaryTree(this.root);
-    return ans;
-  }
+	getDiameter(): number {
+		let ans = 0;
+		function diameterOfBinaryTree(root: TreeNode<T> | null): number {
+			if (!root) {
+				return 0;
+			}
+			const left = diameterOfBinaryTree(root.left);
+			const right = diameterOfBinaryTree(root.right);
+			ans = Math.max(left + right, ans);
+			return 1 + Math.max(left, right);
+		}
+		diameterOfBinaryTree(this.root);
+		return ans;
+	}
 
-  /**
-   * Given a binary tree, determine if it is height-balanced.
-   * <br /><br />
-   * a height-balanced binary tree is defined as:
-   * "a binary tree in which the left and right subtrees of every node differ in height by no more than 1."
-   * For example,
-   * <br />
-   * <b>Input:</b> [3, 9, 20, null, null, 15, 7]
-   * <br />
-   * <b>Output:</b> true
-   * <br />
-   *
-   * @method
-   * @see {@link https://leetcode.com/problems/balanced-binary-tree/|Balanced Binary Tree}
-   *
-   * @public
-   *
-   * @returns {boolean} Tree is balanced or not
-   */
+	/**
+	 * Given a binary tree, determine if it is height-balanced.
+	 * <br /><br />
+	 * a height-balanced binary tree is defined as:
+	 * "a binary tree in which the left and right subtrees of every node differ in height by no more than 1."
+	 * For example,
+	 * <br />
+	 * <b>Input:</b> [3, 9, 20, null, null, 15, 7]
+	 * <br />
+	 * <b>Output:</b> true
+	 * <br />
+	 *
+	 * @method
+	 * @see {@link https://leetcode.com/problems/balanced-binary-tree/|Balanced Binary Tree}
+	 *
+	 * @public
+	 *
+	 * @returns {boolean} Tree is balanced or not
+	 */
 
-  isBalanced(): boolean {
-    function checkBalanced(root: TreeNode<T> | null): number {
-      if (!root) {
-        return 0;
-      }
-      const left = checkBalanced(root.left);
-      if (left === -1) return -1;
-      const right = checkBalanced(root.right);
-      if (right === -1) return -1;
-      return Math.abs(left - right) > 1 ? -1 : 1 + Math.max(left, right);
-    }
-    return checkBalanced(this.root) > -1 ? true : false;
-  }
+	isBalanced(): boolean {
+		function checkBalanced(root: TreeNode<T> | null): number {
+			if (!root) {
+				return 0;
+			}
+			const left = checkBalanced(root.left);
+			if (left === -1) return -1;
+			const right = checkBalanced(root.right);
+			if (right === -1) return -1;
+			return Math.abs(left - right) > 1 ? -1 : 1 + Math.max(left, right);
+		}
+		return checkBalanced(this.root) > -1 ? true : false;
+	}
 
-  /**
-   * Given the roots of two binary trees p and q, write a function to check if they are the same or not.
-   * <br /><br />
-   * Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
-   * For example,
-   * <br />
-   * <b>Input:</b> p = [1, 2, 3], q = [1, 2, 3]
-   * <br />
-   * <b>Output:</b> true
-   * <br />
-   *
-   * @static
-   * @function
-   * @see {@link https://leetcode.com/problems/same-tree/|Same Tree}
-   *
-   * @public
-   *
-   * @returns {boolean} Both trees are same or not
-   */
+	/**
+	 * Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+	 * <br /><br />
+	 * Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+	 * For example,
+	 * <br />
+	 * <b>Input:</b> p = [1, 2, 3], q = [1, 2, 3]
+	 * <br />
+	 * <b>Output:</b> true
+	 * <br />
+	 *
+	 * @static
+	 * @function
+	 * @see {@link https://leetcode.com/problems/same-tree/|Same Tree}
+	 *
+	 * @public
+	 *
+	 * @returns {boolean} Both trees are same or not
+	 */
 
-  static isSameTree<T>(tree1: AVLTree<T>, tree2: AVLTree<T>): boolean {
-    function sameTree(
-      root1: TreeNode<T> | null,
-      root2: TreeNode<T> | null
-    ): boolean {
-      if (!root1 && !root2) return true;
-      if (!root1 || !root2 || root1.value !== root2.value) return false;
-      const isLTreeSame = sameTree(root1.left, root2.left);
-      const isRTreeSame = sameTree(root1.right, root2.right);
-      return isLTreeSame && isRTreeSame;
-    }
-    return sameTree(tree1.root, tree2.root);
-  }
+	static isSameTree<T>(tree1: AVLTree<T>, tree2: AVLTree<T>): boolean {
+		function sameTree(
+			root1: TreeNode<T> | null,
+			root2: TreeNode<T> | null
+		): boolean {
+			if (!root1 && !root2) return true;
+			if (!root1 || !root2 || root1.value !== root2.value) return false;
+			const isLTreeSame = sameTree(root1.left, root2.left);
+			const isRTreeSame = sameTree(root1.right, root2.right);
+			return isLTreeSame && isRTreeSame;
+		}
+		return sameTree(tree1.root, tree2.root);
+	}
 }
--- src/test/ds/avl-tree.test.ts
+++ src/test/ds/avl-tree.test.ts
@@ -1,105 +1,105 @@
 import { TreeNode, AVLTree } from '../../ds/avl-tree';
 describe('Node', () => {
-  it('should  create a Node', () => {
-    const node = new TreeNode('some data', null, null, 0);
-    expect(node.value).toEqual('some data');
-  });
+	it('should  create a Node', () => {
+		const node = new TreeNode('some data', null, null, 0);
+		expect(node.value).toEqual('some data');
+	});
 });
 
 describe('AVLTree', () => {
-  it('should insert items into AVLTree', () => {
-    const tree = new AVLTree<number>();
-    tree.insert(4);
-    tree.insert(2);
-    tree.insert(7);
-    expect(tree.root?.value).toEqual(4);
-    expect(tree.root?.left?.value).toEqual(2);
-    expect(tree.root?.right?.value).toEqual(7);
-  });
+	it('should insert items into AVLTree', () => {
+		const tree = new AVLTree<number>();
+		tree.insert(4);
+		tree.insert(2);
+		tree.insert(7);
+		expect(tree.root?.value).toEqual(4);
+		expect(tree.root?.left?.value).toEqual(2);
+		expect(tree.root?.right?.value).toEqual(7);
+	});
 
-  it('should invert the AVLTree', () => {
-    const tree = new AVLTree<number>();
-    tree.insert(4);
-    tree.insert(2);
-    tree.insert(7);
-    tree.invert();
-    expect(tree.root?.value).toEqual(4);
-    expect(tree.root?.left?.value).toEqual(7);
-    expect(tree.root?.right?.value).toEqual(2);
-  });
+	it('should invert the AVLTree', () => {
+		const tree = new AVLTree<number>();
+		tree.insert(4);
+		tree.insert(2);
+		tree.insert(7);
+		tree.invert();
+		expect(tree.root?.value).toEqual(4);
+		expect(tree.root?.left?.value).toEqual(7);
+		expect(tree.root?.right?.value).toEqual(2);
+	});
 
-  it('should return height the AVLTree', () => {
-    const tree = new AVLTree<number | null>();
-    tree.insert(9);
-    tree.insert(3);
-    tree.insert(20);
-    tree.insert(15);
-    tree.insert(7);
-    expect(tree.getHeight()).toEqual(3);
-  });
+	it('should return height the AVLTree', () => {
+		const tree = new AVLTree<number | null>();
+		tree.insert(9);
+		tree.insert(3);
+		tree.insert(20);
+		tree.insert(15);
+		tree.insert(7);
+		expect(tree.getHeight()).toEqual(3);
+	});
 
-  it('should return diameter of the AVLTree', () => {
-    const tree = new AVLTree<number | null>();
-    tree.insert(5);
-    tree.insert(3);
-    tree.insert(4);
-    tree.insert(2);
-    tree.insert(1);
-    expect(tree.getDiameter()).toEqual(3);
-  });
+	it('should return diameter of the AVLTree', () => {
+		const tree = new AVLTree<number | null>();
+		tree.insert(5);
+		tree.insert(3);
+		tree.insert(4);
+		tree.insert(2);
+		tree.insert(1);
+		expect(tree.getDiameter()).toEqual(3);
+	});
 
-  describe('isBalanced', () => {
-    it('should return true if AVLTree is balanced', () => {
-      const tree = new AVLTree<number | null>();
-      tree.insert(5);
-      tree.insert(6);
-      tree.insert(3);
-      tree.insert(2);
-      expect(tree.isBalanced()).toEqual(true);
-    });
+	describe('isBalanced', () => {
+		it('should return true if AVLTree is balanced', () => {
+			const tree = new AVLTree<number | null>();
+			tree.insert(5);
+			tree.insert(6);
+			tree.insert(3);
+			tree.insert(2);
+			expect(tree.isBalanced()).toEqual(true);
+		});
 
-    it('should return false if AVLTree is not balanced(all nodes right)', () => {
-      const tree = new AVLTree<number | null>();
-      tree.insert(5);
-      tree.insert(6);
-      tree.insert(7);
-      tree.insert(8);
-      expect(tree.isBalanced()).toEqual(false);
-    });
+		it('should return false if AVLTree is not balanced(all nodes right)', () => {
+			const tree = new AVLTree<number | null>();
+			tree.insert(5);
+			tree.insert(6);
+			tree.insert(7);
+			tree.insert(8);
+			expect(tree.isBalanced()).toEqual(false);
+		});
 
-    it('should return false if AVLTree is not balanced(all nodes left)', () => {
-      const tree = new AVLTree<number | null>();
-      tree.insert(5);
-      tree.insert(4);
-      tree.insert(3);
-      tree.insert(2);
-      expect(tree.isBalanced()).toEqual(false);
-    });
-  });
+		it('should return false if AVLTree is not balanced(all nodes left)', () => {
+			const tree = new AVLTree<number | null>();
+			tree.insert(5);
+			tree.insert(4);
+			tree.insert(3);
+			tree.insert(2);
+			expect(tree.isBalanced()).toEqual(false);
+		});
+	});
 
-  describe('isSameTree', () => {
-    it('should return true if both trees are same', () => {
-      const tree1 = new AVLTree<number>();
-      const tree2 = new AVLTree<number>();
-      tree1.insert(2);
-      tree1.insert(1);
-      tree1.insert(3);
-      tree2.insert(2);
-      tree2.insert(1);
-      tree2.insert(3);
-      expect(AVLTree.isSameTree(tree1, tree2)).toEqual(true);
-    });
+	describe('isSameTree', () => {
+		it('should return true if both trees are same', () => {
+			const tree1 = new AVLTree<number>();
+			const tree2 = new AVLTree<number>();
+			tree1.insert(2);
+			tree1.insert(1);
+			tree1.insert(3);
+			tree2.insert(2);
+			tree2.insert(1);
+			tree2.insert(3);
+			expect(AVLTree.isSameTree(tree1, tree2)).toEqual(true);
+		});
 
-    it('should return false if both trees are not same', () => {
-      const tree1 = new AVLTree<number>();
-      const tree2 = new AVLTree<number>();
-      tree1.insert(2);
-      tree1.insert(1);
-      tree1.insert(3);
-      tree2.insert(5);
-      tree2.insert(6);
-      tree2.insert(7);
-      expect(AVLTree.isSameTree(tree1, tree2)).toEqual(false);
-    });
-  });
+		it('should return false if both trees are not same', () => {
+			const tree1 = new AVLTree<number>();
+			const tree2 = new AVLTree<number>();
+			tree1.insert(2);
+			tree1.insert(1);
+			tree1.insert(3);
+			tree2.insert(5);
+			tree2.insert(6);
+			tree2.insert(7);
+			expect(AVLTree.isSameTree(tree1, tree2)).toEqual(false);
+		});
+	});
 });
